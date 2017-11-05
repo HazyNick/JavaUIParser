c19bfe23aeed8e2149cb86e06e960106da061cf1
HazyNick <ang.nik10@gmail.com>
2017-11-05 16:48:27 +0800

    Updated UI.java with working load

--- src/UI.java
+++ src/UI.java
@@ -1,6 +1,57 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import org.json.simple.parser.ParseException;
+import java.io.IOException;
 
 public class UI {
+    private JFrame frame;
     private JPanel mainPane;
     private JButton loadJSON;
+    private JSONStuff getFile;
+
+    public UI() {
+        // Initialize the variables
+        frame = new JFrame("Java UI Parser");
+        mainPane = new JPanel();
+        frame.setContentPane(mainPane);
+        getFile = new JSONStuff();
+
+        loadJSON = new JButton("Load JSON");
+        loadJSON.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+                int result = fileChooser.showOpenDialog(frame);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = fileChooser.getSelectedFile();
+                    try {
+                        getFile.extractData(selectedFile);
+                    } catch (FileNotFoundException e1) {
+                        e1.printStackTrace();
+                    } catch (ParseException e1) {
+                        e1.printStackTrace();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        frame.getContentPane().setLayout(new FlowLayout());
+        frame.add(loadJSON);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(1366, 768));
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        UI u = new UI();
+
+    }
 }
